e18f3312df5e391800f1f41a764c90937d405bb5
Mark Dorsch <dorschmark@gmail.com>
2024-11-05 17:48:01 -0600

    update main

--- __main__.py
+++ __main__.py
@@ -1,14 +1,15 @@
+import manage
 import sys
 import protocol
 from twisted.python import log
-from twisted.internet import reactor, task
+from twisted.internet import reactor, task, ssl
 from autobahn.twisted.websocket import WebSocketServerFactory
 
 
 class GameFactory(WebSocketServerFactory):
     def __init__(self, hostname: str, port: int):
         self.protocol = protocol.GameServerProtocol
-        super().__init__(f"ws://{hostname}:{port}")
+        super().__init__(f"wss://{hostname}:{port}")
 
         self.players: set[protocol.GameServerProtocol] = set()
 
