6b451a2e52c640f83b05e6897b23b082e3e1b5d3
Mark Dorsch <dorschmark@gmail.com>
2024-11-06 21:18:03 -0800

    some text

--- client/websockets_client.gd
+++ client/websockets_client.gd
@@ -11,28 +11,31 @@ signal error
 var socket = WebSocketPeer.new()
 
 func _ready():
-	var hostname = "localhost"
-	var port = 8081
-	var websocket_url = "wss://%s:%d" % [hostname, port]
-	var options = TLSOptions.client_unsafe()
-	var err = socket.connect_to_url(websocket_url, options)
+	var hostname = "cartandpod.com"  # Replace with your actual domain
+	var port = 80  # Use port 80 for HTTP, as Cloudflare handles SSL/TLS
+	var websocket_url = "ws://%s:%d" % [hostname, port]
+	var err = socket.connect_to_url(websocket_url)
 
 	if err != OK:
-		print("Unable to connect")
+		print("Unable to connect, error code: ", err)
 		set_process(false)
 		emit_signal("error")
+	else:
+		print("Connecting to WebSocket...")
 
 func _process(delta):
 	socket.poll()
 	var state = socket.get_ready_state()
 	if state == WebSocketPeer.STATE_OPEN:
+		print("WebSocket is open")
 		while socket.get_available_packet_count():
 			var packet = socket.get_packet()
 			print("Packet: ", packet)
 			data.emit(packet.get_string_from_utf8())
+	elif state == WebSocketPeer.STATE_CONNECTING:
+		print("WebSocket is connecting...")
 	elif state == WebSocketPeer.STATE_CLOSING:
-		# Keep polling to achieve proper close.
-		pass
+		print("WebSocket is closing...")
 	elif state == WebSocketPeer.STATE_CLOSED:
 		var code = socket.get_close_code()
 		var reason = socket.get_close_reason()
@@ -49,3 +52,30 @@ func send_packet(packet: Packet) -> void:
 func _send_string(string: String) -> void:
 	socket.send_text(string)
 	print("Sent string ", string)
+
+# Additional debug function to check connection status
+func check_connection_status():
+	var state = socket.get_ready_state()
+	if state == WebSocketPeer.STATE_CONNECTING:
+		print("WebSocket is still connecting...")
+	elif state == WebSocketPeer.STATE_OPEN:
+		print("WebSocket is open")
+	elif state == WebSocketPeer.STATE_CLOSING:
+		print("WebSocket is closing...")
+	elif state == WebSocketPeer.STATE_CLOSED:
+		print("WebSocket is closed")
+
+# Call this function periodically to check the connection status
+func _process2(delta):
+	socket.poll()
+	check_connection_status()
+	if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
+		while socket.get_available_packet_count():
+			var packet = socket.get_packet()
+			print("Packet: ", packet)
+			data.emit(packet.get_string_from_utf8())
+	elif socket.get_ready_state() == WebSocketPeer.STATE_CLOSED:
+		var code = socket.get_close_code()
+		var reason = socket.get_close_reason()
+		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+		set_process(false) # Stop processing.
